12f9b2ada4a0c05d8833a4485095fe728d0c923e
yudong.shao <yudong.shao@qunar.com>
2019-04-18 21:36:12 +0800

    feat: v0.1.1 修复type判断bug

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const { compress } = require('./utils');
 
 module.exports = function({ queues = [], exportCode = '' }, map, meta) {
-    queues.forEach(({ code, type }, index) => {
+    queues.forEach(({ code, type, path: filePath }, index) => {
+        type = type || filePath.match(/\.(\w+)$/)[1];
         queues[index].code = typeof compress[type] === 'function' && compress[type](code) || code;
     });
+    
     this.callback(null, {
         queues,
         exportCode
